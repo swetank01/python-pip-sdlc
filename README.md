8128cc31655881f5b968d65af74d17d6a0ad16aa
swetank soni <iamswetanksoni@gmail.com>
2024-05-22 08:59:03 +0000

    add sample

--- README.md
+++ README.md
@@ -1,3 +1,13 @@
 # Python pip SDLC
 
 - make a pip installable
+
+## Pre-requisite
+
+- pip install wheel
+
+### Pip build 
+
+python3 setup.py bdist_egg --exclude-source-files
+
+python3 -m wheel convert 
---
+++ setup.py
@@ -0,0 +1,16 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='my_microservice',
+    version='0.1.0',
+    packages=find_packages(),
+    include_package_data=True,
+    install_requires=[
+        'Flask',
+    ],
+    entry_points={
+        'console_scripts': [
+            'run-my-microservice=my_microservice.app:app.run',
+        ],
+    },
+)
